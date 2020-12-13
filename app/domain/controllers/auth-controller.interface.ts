7889db603fb6f6ab00858ee1546af093dc98c3ca
adeoluwa-adebiyi <adeoluwaadebiyi@gmail.com>
2020-12-13 16:00:56 +0100

    save changes

--- app/domain/controllers/auth-controller.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Controller } from "../../common/core/contracts/controller.interface";
-import { UserJSON } from "../entities/user.model";
-
-export interface AuthControllerSpec extends Controller<any>{
-    registerUser(userCredentials:any):Promise<UserJSON>;
-    generateUserAuthToken(username:string, password: string): string;
-    refreshUserToken(refreshToken:string): string;
-    generateRoutes():any;
-}
