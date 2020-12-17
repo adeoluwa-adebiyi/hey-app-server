a99e1a396eff00a492454a6c33de2a0f5ba7b507
adeoluwa-adebiyi <adeoluwaadebiyi@gmail.com>
2020-12-17 22:04:32 +0100

    add jwt auth

--- app/routes/express/auth.route.ts
+++ app/routes/express/auth.route.ts
@@ -2,10 +2,20 @@ import "reflect-metadata";
 import { Router, Request, Response } from "express";
 import { AuthenticateUserUsecase } from "../../domain/usecases/authenticate-users.usecase";
 import { InvalidLoginCredentialsException } from "../../common/exceptions/invalid-login-credentials.exception";
+import { ACCESS_JWT_COOKIE_NAME, JWT_COOKIE_EXPIRY, REFRESH_JWT_COOKIE_NAME } from "../../config/app.config";
 
 export const AUTH_USER_ROUTE_ENDPOINT:string = "/auth";
 
+export const AUTH_WEB_USER_ROUTE_ENDPOINT:string = "/auth/web";
+
+
 const router: Router = Router();
+
+const AUTH_COOKIE_NAME = ACCESS_JWT_COOKIE_NAME;
+
+const REFRESH_AUTH_COOKIE_NAME = REFRESH_JWT_COOKIE_NAME;
+
+const AUTH_COOKIE_EXPIRY = JWT_COOKIE_EXPIRY;
 
 router.post("/", async(req:Request, res: Response)=>{
     try{
@@ -21,6 +31,53 @@ router.post("/", async(req:Request, res: Response)=>{
             res.send({
                 error: e.constructor.name,
                 message: e.message
+            });
+        }
+        else{
+            res.status(500).send({
+                error: "Server error",
+                message: ""
+            });
+        }
+    }
+});
+
+
+router.post("/web", async(req:Request, res: Response)=>{
+    try{
+        const params = req.body;
+        const response = await new AuthenticateUserUsecase().execute({...params});
+        res.cookie(AUTH_COOKIE_NAME, response.accessToken, {maxAge: AUTH_COOKIE_EXPIRY, httpOnly: true, secure: true });
+        res.cookie(REFRESH_AUTH_COOKIE_NAME, response.refreshToken, {maxAge: AUTH_COOKIE_EXPIRY, httpOnly: true, secure: true });
+        res.sendStatus(200);
+    }catch(e){
+        console.log(e)
+        if(e instanceof InvalidLoginCredentialsException){
+            res.status(401).send({
+                error: "Authentication Failed",
+                message: "Invalid login attempt"
+            });
+        }
+        else{
+            res.status(500).send({
+                error: "Server error",
+                message: ""
+            });
+        }
+    }
+});
+
+
+router.post("/test", async(req:Request, res: Response)=>{
+    try{
+        console.log("ENDPOINT");
+        res.sendStatus(200);
+    }catch(e){
+        console.log(e)
+        if(e instanceof InvalidLoginCredentialsException){
+            res.status(401).send({
+                error: "Authentication Failed",
+                message: "Invalid login attempt"
             });
         }
         else{
